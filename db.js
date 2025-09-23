38fb96fada3fb9eb3cfa4940f0e1ac66fa3bcfe2
J0sF3r <jdiazm17@miumg.edu.gt>
2025-09-22 22:19:03 -0600

    Cambios en db

--- db.js
+++ db.js
@@ -16,6 +16,7 @@ module.exports = {
 
 // backend/db.js render
 // backend/db.js
+// backend/db.js
 const { Pool } = require('pg');
 
 // Solo carga dotenv si NO estamos en producción
@@ -23,27 +24,14 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = process.env.NODE_ENV === 'production'
+    ? process.env.DATABASE_URL
+    : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-// Objeto de configuración que cambiará según el entorno
-const connectionConfig = isProduction
-    ? { // Configuración para Render usando las variables separadas
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT,
-        ssl: { rejectUnauthorized: false }
-      }
-    : { // Configuración para tu máquina Local
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT
-      };
-
-const pool = new Pool(connectionConfig);
+// El objeto de configuración ahora es mucho más simple
+const pool = new Pool({
+    connectionString: connectionString,
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
